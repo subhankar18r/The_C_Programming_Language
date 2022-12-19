e154a40ecc850681da6bfb62d3d58eefefe576d8
subhankar18r <subhankar18r@gmail.com>
2022-12-19 19:31:54 +0530

    initial commit

--- 02_loops & conditionals/06_sumOfNaturalNumber.c
+++ 02_loops & conditionals/06_sumOfNaturalNumber.c
@@ -10,16 +10,11 @@ int main()
 
     for (int i = 1, j = a; i <= a; i++, j--)
     {
-        printf("%d\n", j);
+
         sum += i;
     }
 
     printf("%d\n", sum);
-    // for (int i = a; i >= 1; i--)
-    // {
-    //     sum += i;
-    //     printf("%d\n", i);
-    // }
 
     return 0;
 }
